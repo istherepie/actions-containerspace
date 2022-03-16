fe1da590bfe91f8b4d1ff8afe9089e69cfccdd61
Steffen Park <dev@istherepie.com>
2022-03-17 00:15:23 +0100

    Create container for testing

---
+++ container/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Checking the directories available!"
+
+ls /home/runner/work/_temp/_github_home
+ls /home/runner/work/actions-containerspace/actions-containerspace
+ls /github/workspace
+
